646a46212606d5dcb65646e5b90a9b2b301132ed
Aux-028 <dinothan1@gmail.com>
2020-12-22 10:32:15 +0530

    Color picker for React Native / React JS / React native for Web

---
+++ LinearGradient/LinearGradient.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import Uranium from 'uranium';
+
+const LinearGradientCommon = props => {
+  return (
+    <LinearGradient
+      colors={props.colors}
+      style={props.style}
+      start={props.start}
+      end={props.end}>
+      {props.children}
+    </LinearGradient>
+  );
+};
+
+export default Uranium(LinearGradientCommon);
